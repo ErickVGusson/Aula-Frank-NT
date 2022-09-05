0a2949952c240cb47fe1faaba685c40fa849a335
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2022-09-05 19:17:36 -0300

    Aula 5 - 2

--- Aula 5/Aula 5 - 2/script.js
+++ Aula 5/Aula 5 - 2/script.js
@@ -1,5 +1,18 @@
-function media() {
+function calcular() {
 
-    var nota1 = Number(document.getElementById("nota1").value)
-    var nota2 = Number(document.getElementById("nota2").value)
+    var nota1 = Number(document.getElementsByTagName('input')[0].value)
+    var nota2 = Number(document.getElementsByTagName('input')[1].value)
+
+    var media = (nota1 + nota2) / 2;
+
+    if (media >= 7 && media < 10) {
+        document.querySelector('p').innerHTML = `O aluno esta aprovado.`
+    }
+    else if (media >= 10) {
+        document.querySelector('p').innerHTML = `O aluno tirou a nota <strong style="color: red;">${media}</strong> e esta aprovado com distinção.`
+    }
+    else {
+        document.querySelector('p').innerHTML = `O aluno esta reprovado.`
+    }
+
 }
